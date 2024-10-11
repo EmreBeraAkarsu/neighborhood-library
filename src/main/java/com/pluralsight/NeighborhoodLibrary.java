bfa178834bb3926ac12deaa362e8c19a980f0e0b
Emre Akarsu <akarsutech@gmail.com>
2024-10-10 23:55:18 -0400

    exercise neighborhoodLibrary, created the Book class and some book objects

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,4 +1,59 @@
 package com.pluralsight;
 
 public class NeighborhoodLibrary {
+    public static void main(String[] args) {
+        Book[] books = new Book[20];
+
+
+
+        Book book1 = new Book(123, "1252345234513", "Book1", false, "");
+        Book book2 = new Book(35265, "37434736473467", "Book2", false, "");
+        Book book3 = new Book(11234523, "67367367346573456", "Book3", false, "");
+        Book book4 = new Book(4646, "78658658678", "Book4", false, "");
+        Book book5 = new Book(1223453, "4524354352352345", "Book5", false, "");
+        Book book6 = new Book(545435, "4567342562", "Book6", false, "");
+        Book book7 = new Book(12636563, "7656845678536", "Book7", false, "");
+        Book book8 = new Book(7747754, "62356233725345", "Book8", false, "");
+        Book book9 = new Book(124443, "8736234562", "Book9", false, "");
+        Book book10 = new Book(2222, "123455677", "Book10", true, "Mehmet");
+        Book book11 = new Book(1446423, "42376567543267245234", "Book11", true, "Lavuk");
+        Book book12 = new Book(166623, "78347456234652", "Book12", true, "kes");
+        Book book13 = new Book(188823, "785348568563734", "Book13", true, "Leylek");
+        Book book14 = new Book(127773, "2351432551345", "Book14", true, "Naber");
+        Book book15 = new Book(1545523, "7324573473467347", "Book15", true, "Raymond");
+        Book book16 = new Book(1244443, "347643734674788", "Book16", true, "nevar");
+        Book book17 = new Book(1333323, "53462566675324", "Book17", true, "oyle");
+        Book book18 = new Book(1000023, "73456236236", "Book18", true, "yeter");
+        Book book19 = new Book(1215874373, "31313131313331", "Book19", true, "java");
+        Book book20 = new Book(145854523, "6446466464664664", "Book20", true, "bugun");
+
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+        books[4] = book4;
+        books[5] = book5;
+        books[6] = book6;
+        books[7] = book7;
+        books[8] = book8;
+        books[9] = book9;
+        books[0] = book10;
+        books[10] = book11;
+        books[11] = book12;
+        books[12] = book13;
+        books[13] = book14;
+        books[14] = book15;
+        books[15] = book16;
+        books[16] = book17;
+        books[17] = book18;
+        books[18] = book19;
+        books[19] = book20;
+
+        while (true){
+
+            
+        }
+
+    }
+
+
 }
